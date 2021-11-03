dd98cd5675d5e497747d336dd544262a3efa86b1
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-03 17:53:27 -0600

    Add a new method for a word

--- decode.rb
+++ decode.rb
@@ -1,19 +1,32 @@
-@morse = '·–', '–···',	'–·–·',	'–··',	'·',	'··–·',	'––·',	'····',	'··',	'·–––',	'–·–',
-  '·–··','––',	'–·', '–––',	'––·–',	'·––·',	'·–·',	'···',	'–', '··–',	'···–',
-  '·––',	'–··–',	'–·––',	'––··'
+@morse = '.-', '-...',	'-.-.',	'-..',	'.',	'..-.',	'--.',	'....',	'..',	'.---',	'-.-',
+         '.-..', '--',	'-.', '---',	'--.-',	'.--.',	'.-.',	'...',	'-', '..-',	'...-',
+         '.--',	'-..-',	'-.--',	'--..'
 
 @alphabet = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-  'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 
 def decode_char(char)
   @morse.each_with_index do |m, idx|
-    if char == m
-      @alphabet.each_with_index { |letter,i |
-        if i == idx
-          puts(letter)
-        end}
-    end
-    end
-end
-decode_char('··–·')
+    next unless char == m
 
+    @alphabet.each_with_index do |letter, i|
+      puts(letter) if i == idx
+    end
+  end
+end
+decode_char('.-')
+
+def decode_word(word)
+  word.split.each_with_index do |t, _i|
+    @morse.each_with_index do |m, idx|
+      next unless t == m
+
+      @alphabet.each_with_index do |letter, i|
+
+        puts(letter) if i == idx
+      end
+    end
+  end
+end
+
+decode_word('-- -.--')
