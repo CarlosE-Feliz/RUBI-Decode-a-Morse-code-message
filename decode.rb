bba08df4f94c1ed67b77c7d02b4eb4b6c8492956
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-04 21:42:01 +0500

    Fix liter errors

--- decode.rb
+++ decode.rb
@@ -1,35 +1,31 @@
- @morse = '.-', '-...',	'-.-.',	'-..',	'.',	'..-.',	'--.',	'....',	'..',	'.---',	'-.-',
-             '.-..', '--',	'-.', '---',	'--.-',	'.--.',	'.-.',	'...',	'-', '..-',	'...-',
-             '.--',	'-..-',	'-.--',	'--..'
+@morse = '.-', '-...',	'-.-.',	'-..',	'.',	'..-.',	'--.',	'....',	'..',	'.---',	'-.-',
+         '.-..', '--',	'-.', '---',	'--.-',	'.--.',	'.-.',	'...',	'-', '..-',	'...-',
+         '.--',	'-..-',	'-.--',	'--..'
 
- @alphabet = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-             'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+@alphabet = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 
- def decode_char(char)
-   @morse.each_with_index do |m, idx|
-     next unless char == m
+def decode_char(char)
+  @morse.each_with_index do |m, idx|
+    next unless char == m
 
-     @alphabet.each_with_index do |letter, i|
-       puts(letter) if i == idx
-     end
-   end
- end
+    @alphabet.each_with_index do |letter, i|
+      puts(letter) if i == idx
+    end
+  end
+end
 
+def decode_word(word)
+  fullWord = ''
+  split_word = word.split(' ')
+  split_word.each_with_index do |t, _i|
+    @morse.each_with_index do |m, idx|
+      next unless t == m
 
-
- def decode_word(word)
-   fullWord = ''
-   split_word = word.split(' ')
-   split_word.each_with_index do |t, _i|
-     @morse.each_with_index do |m, idx|
-       next unless t == m
-
-       @alphabet.each_with_index do |letter, i|
-         fullWord += letter if i == idx
-       end
-     end
-   end
-   puts(fullWord.split('').join(' '))
- end
-
-
+      @alphabet.each_with_index do |letter, i|
+        fullWord += letter if i == idx
+      end
+    end
+  end
+  puts(fullWord.split('').join(' '))
+end
