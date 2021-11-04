23e3fb0a39ebc9a022734400b214f9bcee7da201
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-03 18:06:41 -0600

    Add a new method for a word

--- decode.rb
+++ decode.rb
@@ -5,28 +5,31 @@
 @alphabet = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
             'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 
-def decode_char(char)
-  @morse.each_with_index do |m, idx|
-    next unless char == m
+# def decode_char(char)
+#   @morse.each_with_index do |m, idx|
+#     next unless char == m
+#
+#     @alphabet.each_with_index do |letter, i|
+#       puts(letter) if i == idx
+#     end
+#   end
+# end
+# decode_char('.-')
 
-    @alphabet.each_with_index do |letter, i|
-      puts(letter) if i == idx
-    end
-  end
-end
-decode_char('.-')
 
 def decode_word(word)
+  @@complete = ''
   word.split.each_with_index do |t, _i|
     @morse.each_with_index do |m, idx|
       next unless t == m
 
       @alphabet.each_with_index do |letter, i|
 
-        puts(letter) if i == idx
+        @@complete += letter if i == idx
       end
     end
   end
+  puts(@@complete)
 end
 
 decode_word('-- -.--')
