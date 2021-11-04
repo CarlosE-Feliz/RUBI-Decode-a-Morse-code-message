a45c70a3411491a8fccd6947bfa07ce74f2b7454
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-04 08:30:59 -0600

    update method

--- decode.rb
+++ decode.rb
@@ -1,35 +1,35 @@
-@morse = '.-', '-...',	'-.-.',	'-..',	'.',	'..-.',	'--.',	'....',	'..',	'.---',	'-.-',
-         '.-..', '--',	'-.', '---',	'--.-',	'.--.',	'.-.',	'...',	'-', '..-',	'...-',
-         '.--',	'-..-',	'-.--',	'--..'
+ @morse = '.-', '-...',	'-.-.',	'-..',	'.',	'..-.',	'--.',	'....',	'..',	'.---',	'-.-',
+             '.-..', '--',	'-.', '---',	'--.-',	'.--.',	'.-.',	'...',	'-', '..-',	'...-',
+             '.--',	'-..-',	'-.--',	'--..'
 
-@alphabet = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+ @alphabet = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+             'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 
-def decode_char(char)
-  @morse.each_with_index do |m, idx|
-    next unless char == m
+ def decode_char(char)
+   @morse.each_with_index do |m, idx|
+     next unless char == m
 
-    @alphabet.each_with_index do |letter, i|
-      puts(letter) if i == idx
-    end
-  end
-end
-decode_char('.-')
+     @alphabet.each_with_index do |letter, i|
+       puts(letter) if i == idx
+     end
+   end
+ end
 
 
-def decode_word(word)
-  @@complete = ''
-  word.split.each_with_index do |t, _i|
-    @morse.each_with_index do |m, idx|
-      next unless t == m
 
-      @alphabet.each_with_index do |letter, i|
+ def decode_word(word)
+   fullWord = ' '
+   word.split.each_with_index do |t, _i|
+     @morse.each_with_index do |m, idx|
+       next unless t == m
 
-        @@complete += letter if i == idx
-      end
-    end
-  end
-  puts(@@complete)
-end
+       @alphabet.each_with_index do |letter, i|
 
-decode_word('-- -.--')
+         fullWord += letter if i == idx
+       end
+     end
+   end
+   puts(fullWord)
+ end
+
+ decode_word('-- -.--')
